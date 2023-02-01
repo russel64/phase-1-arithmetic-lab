df486c529c49b29ff429f77035cb989ea427698e
William Russell <williamrussell@Williams-MBP.lan>
2023-02-01 01:55:01 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 22
+const num4 = 6
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
+
